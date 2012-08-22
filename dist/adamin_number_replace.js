2388898e13b9a15df41f5a2802b1d6575600ccaf
Adam L <git@adamin.net>
2012-08-21 18:49:36 -0700

    Added dist/

---
+++ dist/adamin_number_replace.js
@@ -0,0 +1,118 @@
+/*! Adamin Number Replace - v0.1.0 - 2012-08-21
+* Copyright (c) 2012 Adam L.; Licensed MIT, GPL */
+
+(function($) {
+
+  var adaminNumReplace = {
+    init: function(el, config) {
+      this.config = $.extend({}, $.fn.adaminNumReplace.defaults, config);
+
+      this.el = el;
+      this.$el = $(el);
+
+      this.updateHTML();
+    },
+
+    updateHTML: function() {
+
+      var self = this;
+
+      var newImages = $('img', this.$el);
+
+      var contentText = this.$el.html();
+
+      var cleanTextValues = this.cleanText( contentText );
+
+      this.$el.
+        html(function(i, text){
+          return cleanTextValues[0];
+        }).
+        addClass(cleanTextValues[1])
+        ;
+
+      // add imgClass if config is set
+      if (this.config.margin) {
+        $('img', this.$el).css({
+          'margin': this.config.margin
+        });
+      }
+
+      // add imgClass if config is set
+      if (this.config.imgClass) {
+        $('img', this.$el).addClass(this.config.imgClass);
+      }
+
+      // add text into element and Nicolas Gallagher IR Technique
+      $('<span>', {
+        text: cleanTextValues[1]
+      })
+        .css({
+          'font': '0/0 a',
+          'text-shadow': 'none',
+          'color': 'transparent'
+      })
+        .appendTo(this.$el);
+
+    },
+
+    cleanText: function( text ) {
+      // remove all non numbers, commas or periods
+      var cleanText = text.replace(/[^\d.,!+]/g, "");
+
+      var textArray, imgText;
+
+      // convert text to array
+      textArray = text.split('');
+
+      // convert array to image tags
+      for (var i = 0; i < textArray.length; i++) {
+
+        switch (textArray[i]) {
+          case ",":
+            textArray[i] = 'comma';
+            break;
+          case ".":
+            textArray[i] = 'period';
+            break;
+          case "!":
+            textArray[i] = 'exclamation';
+            break;
+          case "+":
+            textArray[i] = 'plus';
+            break;
+          default:
+            textArray[i] = textArray[i];
+        }
+
+        textArray[i] = '<img src="' + this.config.path + this.config.prefix + textArray[i] + this.config.suffix + '" alt="' + textArray[i] + '">';
+      }
+
+      // join image tags back into string
+      imgText = textArray.join('');
+      
+      // pass complete text back to updateHTML
+      return [imgText, cleanText];
+
+    }
+
+  };
+
+  // Collection method.
+  $.fn.adaminNumReplace = function(config) {
+    var obj = Object.create(adaminNumReplace);
+    return this.each(function() {
+      obj.init(this, config);
+    });
+  };
+
+  // Set defaults
+  $.fn.adaminNumReplace.defaults = {
+    path: '../images/small/',
+    prefix: '',
+    suffix: '.png',
+    margin: '',
+    imgClass: ''
+  };
+
+
+}(jQuery));
